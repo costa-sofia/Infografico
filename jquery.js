c975dc4faad5be4a23b18bd9b000a9564dbd59d9
PedroMaxis <125074965+PedroMaxis@users.noreply.github.com>
2023-11-14 16:28:57 -0300

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -2,10 +2,11 @@
 //fechar o balao de info com o esc
 $(document).ready(function(){
 
+
    let elem = "#info"; 
 $( document ).on( 'keydown', function ( e ) {
     if ( e.keyCode === 27 ) { // ESC
-        $( elem ).hide();
+        $(elem).hide();
     }
 });
 })

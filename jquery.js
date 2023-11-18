dfd04b262b5edf8f9b0637b1fcae8465113132c9
costa-sofia <134660626+costa-sofia@users.noreply.github.com>
2023-11-18 12:04:10 -0300

    Project Completed

--- jquery.js
+++ jquery.js
@@ -1,12 +1,19 @@
 
-//fechar o balao de info com o esc
+//fechar o balao de info com o esc e clickando fora
 $(document).ready(function(){
+    const container = $("#info");
+    $(document).mouseup(function(e){
+    
+        // checa se o click nao foi no #info
+        if(!container.is(e.target) && container.has(e.target).length === 0){
+            container.hide();
+        }
+    });
 
-
-   let elem = "#info"; 
 $( document ).on( 'keydown', function ( e ) {
+    
     if ( e.keyCode === 27 ) { // ESC
-        $(elem).hide();
+        $(container).hide();
     }
 });
 })
